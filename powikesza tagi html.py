7e9dbba9713cb6ce6eacbdd5e1df3b819881175f
grands14320 <grands14320@gmail.com>
2017-02-28 13:15:38 +0100

    Add files via upload

---
+++ powikesza tagi html.py
@@ -0,0 +1,66 @@
+# html = str(input())
+# schowek = []
+# licznik = 0
+# for i in range(0,len(html)):
+#     if len(html) == i + licznik:
+#         break
+#     if html[i + licznik] == "<":
+#         schowek.append(html[i + licznik])
+#         j = i + licznik
+#         while html[j] != '>':
+#             j = j + 1
+#             schowek.append(html[j].upper())
+#             licznik = licznik + 1
+#     else:
+#         i = i + licznik
+#         schowek.append(html[i])
+# print(schowek)
+# print (''.join(schowek))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+schowek = []
+licznik = 0
+with open(r"D:\zadanie.txt", "r+") as f:
+    odczyt = f.readlines()
+    print()
+    for zdanie in odczyt:
+        licznik = 0
+        for litera in range(0, len(zdanie)):
+            if len(zdanie) == litera + licznik:
+
+                break
+            if zdanie[litera + licznik] == "<":
+                schowek.append(zdanie[litera + licznik])
+                j = litera + licznik
+                while zdanie[j] != '>':
+                    j = j + 1
+                    schowek.append(zdanie[j].upper())
+                    licznik = licznik + 1
+            else:
+                litera = litera + licznik
+                schowek.append(zdanie[litera])
+    print(schowek)
+    print("".join(schowek),end='')
